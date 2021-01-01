855a77e4244a6f3d99b4c5db350c18afed091bb0
unknown <jklimpf@gmail.com>
2021-01-01 16:12:32 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,182 @@
+//Učitavanje potrebnih npm modula
+const mqtt = require('mqtt')
+const dotenv = require('dotenv')
+const mongodb = require('mongodb')
+const {sendWarningMailSoba1,sendWarningMailSoba2, sensor1Warning, sensor2Warning, dbError} = require ('./mail')
+const express = require('express')
+const path = require('path')
+const hbs = require ('hbs')
+
+//učitavanje config direktorija
+dotenv.config({path: './config/dev.env'}) 
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+const webDirectoryPath = path.join(__dirname, './web')
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(webDirectoryPath))
+
+var temp1, temp2, gasSensor, gasSensor2                
+var poslanoSoba1 = false
+var poslanoSoba2 = false
+var poslanoSenzor1 = false
+var poslanoSenzor2 = false
+var poslanoDB = false
+
+// Definiranje baze podataka (mongoDB)
+const MongoClient = mongodb.MongoClient                     
+
+const connectionURL = process.env.MONGO_URI
+const databaseName = 'node_dht_projekt'
+
+//Provjeri uvjete (ako su zadovoljeni pošalji obavijest, ali samo jedanput)
+const conditions = () =>{
+    if(temp1 === 'nan') {
+        if(poslanoSenzor1 === false){
+             sensor1Warning()
+             poslanoSenzor1 = true
+         }
+     }
+    else poslanoSenzor1 = false
+
+    if(temp2 === 'nan') {
+        if(poslanoSenzor2 === false){
+             sensor2Warning()
+             poslanoSenzor2 = true
+         }
+     }
+    else  poslanoSenzor2 = false
+
+    if(temp1 > 27 || gasSensor > 800) {
+        if(poslanoSoba1 === false){
+            sendWarningMailSoba1()
+
+            // Ako je alarm uključen (uvjet zadovoljen) spremi taj događaj u bazu podataka
+            MongoClient.connect(connectionURL, { useNewUrlParser: true,  useUnifiedTopology: true }, (error, client) =>{
+                const db = client.db(databaseName)
+            db.collection('alarmi').insertOne({
+                soba: 'soba 1',
+                date: new Date()
+            })})
+            poslanoSoba1 = true
+        }
+    }
+
+    else poslanoSoba1 = false
+
+    if(temp2 > 27 || gasSensor2 > 2000) {
+        if(poslanoSoba2 === false){
+            sendWarningMailSoba2()
+
+            // Ako je alarm uključen (uvjet zadovoljen) spremi taj događaj u bazu podataka
+            MongoClient.connect(connectionURL, { useNewUrlParser: true,  useUnifiedTopology: true }, (error, client) =>{
+                const db = client.db(databaseName)
+            db.collection('alarmi').insertOne({
+                soba: 'soba 2',
+                date: new Date()
+            })})
+            poslanoSoba2 = true
+        }
+    }
+    else poslanoSoba2 = false
+}
+
+const client = mqtt.connect('mqtt://mqtt.thingspeak.com', {             //spajanje na mqtt
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+})
+
+const topic = process.env.TOPIC
+
+client.on('connect', function() {
+    console.log('connected')
+
+    client.subscribe(topic, function(err) {
+        if(! err) {
+            console.log('subscribed')      
+        }
+    })
+})
+
+client.on('message', function(topic, message) {
+
+    //raščlaniti JSON primljen s ThingSpeaka 
+    const parsedMsg = JSON.parse(message)
+         temp1 = parsedMsg.field1
+         temp2 = parsedMsg.field2
+         gasSensor = parsedMsg.field3
+         gasSensor2 = parsedMsg.field4
+
+         //ispitati uvjete (temperatura i plin)
+         conditions()   
+    
+         //spremanje svakog podatka u bazu podataka 
+    MongoClient.connect(connectionURL, { useNewUrlParser: true,  useUnifiedTopology: true }, (error, client) =>{
+        if(error) {
+            if(poslanoDB === false){
+                dbError()
+                poslanoDB = true
+            }
+            return console.log('Unable to connect to database!')
+         }
+         else poslanoDB = false
+         
+         const db = client.db(databaseName)
+ 
+         db.collection('soba1').insertOne({
+             temperatura: temp1.toString(),
+             plin: gasSensor.toString(),
+             date: new Date(),
+         })    
+        
+         db.collection('soba2').insertOne({
+            temperatura: temp2.toString(),
+            plin: gasSensor2.toString(),
+            date: new Date()
+        })    
+    })
+    console.log('Temperatura1 = '+temp1 + '   Plin1 = '+gasSensor + '   Temperatura2 = '+temp2 + '   Plin2 = '+gasSensor2)
+})
+
+//routevi za web stranicu
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'O stranici'
+    })
+})
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Početna'
+       
+    })
+})
+
+app.get('/soba1', (req, res) => {
+    res.render('soba1', {
+        title: 'Soba 1',
+        temp1: temp1,
+        gasSensor: gasSensor,
+        
+    })
+})
+
+app.get('/soba2', (req, res) => {
+    res.render('soba2', {
+        title: 'Soba 2',
+        temp2: temp2,
+        gasSensor2: gasSensor2,
+        
+    })
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port} ` )
+})
---
+++ mail.js
@@ -0,0 +1,85 @@
+const sgMail = require('@sendgrid/mail')
+const dotenv = require('dotenv')
+
+//učitavanje config direktorija
+dotenv.config({path: './config/dev.env'})
+
+const sendgridAPIKey = process.env.SENDGRID_API_KEY
+sgMail.setApiKey(sendgridAPIKey)
+
+//definiranje funkcija za slanje mail-ova
+const sendWarningMailSoba1 = () => {
+    sgMail.send({
+        to: 'josipjosip1411@gmail.com',
+        from: 'josipk1411@gmail.com',
+        subject: 'Upozorenje za sobu 1!',
+        text: 'Visoka temperatura ili kocentracija plina u sobi 1!'
+    }).then(() => {
+        console.log('\nTemperatura ili koncentracija plina u sobi 1 je previsoka.\nPoslana poruka.\n')
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+const sendWarningMailSoba2 = () => {
+    sgMail.send({
+        to: 'josipjosip1411@gmail.com',
+        from: 'josipk1411@gmail.com',
+        subject: 'Upozorenje za sobu 2!',
+        text: 'Visoka temperatura ili kocentracija plina u sobi 2!'
+    }).then(() => {
+        console.log('\nTemperatura ili koncentracija plina u sobi 2 previsoka.\nPoslana poruka.\n')
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+const sensor1Warning = () => {
+    sgMail.send({
+        to: 'josipjosip1411@gmail.com',
+        from: 'josipk1411@gmail.com',
+        subject: 'Neispravnost senzora 1',
+        text: 'Senzor1 ne bilježi temperaturu!'
+    }).then(() => {
+        console.log('\nSenzor1 ne bilježi temperaturu.\nPoslana poruka.\n')
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+const sensor2Warning = () => {
+    sgMail.send({
+        to: 'josipjosip1411@gmail.com',
+        from: 'josipk1411@gmail.com',
+        subject: 'Neispravnost senzora 2',
+        text: 'Senzor2 ne bilježi temperaturu!'
+    }).then(() => {
+        console.log('\nSenzor2 ne bilježi temperaturu.\nPoslana poruka.\n')
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+const dbError = () => {
+    sgMail.send({
+        to: 'josipjosip1411@gmail.com',
+        from: 'josipk1411@gmail.com',
+        subject: 'Nemogucnost spajanja s bazom podataka',
+        text: 'Podaci se ne salju u bazu podataka, provjeriti grešku!'
+    }).then(() => {
+        console.log('\nPoslano upozorenje za gresku baze podataka.\n')
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+module.exports = {
+    sendWarningMailSoba1: sendWarningMailSoba1,
+    sendWarningMailSoba2: sendWarningMailSoba2,
+    sensor1Warning: sensor1Warning,
+    sensor2Warning: sensor2Warning,
+    dbError: dbError
+}
+
+
+
